a2a0a27a16d597f68a40e5d8fe2c77fdc8d0f660
yashpatelSIT <yash.patel.btech2023@sitpune.edu.in>
2026-07-31 15:25:49 +0530

    Add files via upload

---
+++ 23070122235_YashPatel/FlaskApp/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+print("Welcome to the Flask Application")
+print("User Authentication Feature")
+
+@app.route('/')
+def home():
+    return "Hello World!"
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000, debug=True)
